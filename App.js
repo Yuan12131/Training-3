a4fba6f0afe16d52930387ea20e06c35d8a90a55
leeyuan <dbdks32123@gmail.com>
2023-12-01 18:07:56 +0900

    react App.js

---
+++ App.js
@@ -0,0 +1,37 @@
+// App.js
+import React from 'react';
+import './App.css'; // 스타일 파일을 불러옵니다.
+
+function App() {
+  return (
+    <div id="root">
+
+      <div className="upBanner">
+        <div className="upBox">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt autem eaque vitae velit dolor quasi tempora nihil, voluptatem sit, mollitia veniam doloribus quis ab placeat facere? Expedita sunt corporis totam officia adipisci aperiam, minima quos harum incidunt error nihil, nobis atque. <p style={{ color: 'red' }}>Odit dolorem aspernatur reiciendis a architecto sint non illo, explicoremque? Eum, eius v</p>elit doloribus maxime magnam dicta a. Lorem ipsum dolor sit amet consectetur adipisicing elit. Volup nobis atque. Odit dolorem aspernatur reiciendis a architecto sint non illo, explicoremque? Eum, eius velit doloribus maxime magnam dicta a. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, consequuntur! Recusandae ti! Tempore ratione sap?</p>
+        </div>
+        <div className="upMidBox">
+          <h2>HTML TRAINING3</h2>
+          <p>Lorem ipsum dolor sit amet cont sint assumenda quo laboriosam, possimus explicabo est voluptatum dolorum ex? Quasi quisquam aperiam tempora pariatur officia. Ipsa in tempore vero obcaecati sapiente erroruptatum dolorum ex? Quasi quisquam aperiam tempora pariatur officia. Ipsa in tempore vero obcaecati sapiente errouptatum dolorum ex? Quasi quisquam aperiam tempora pariatur officia. Ipsa in tempore vero obcaecati sapiente erro nesciunt reiciendis quos, odit vel voluptatibus quis accusantium exercitationem nostrum non eos quisquam fuga repellendus, voluptatum culpa, quibusdam dolores officiis pariatur eaque! Impedit beatae libero, doloremque delectus ab veritatis! Fugiat dolorem, perferendis facilis quidem, rem laudantium doloremque reprehenderit quas, eum deserunt nemo excepturi earum ipsam pariatur assumenda</p>
+          <h3>Lorem ipsum dolor sit amet. Nul?</h3>
+        </div>
+      </div>
+
+      <div className="downBanner">
+        <div className="downBox">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum et in possimus rerum, eos est exercitationem, itaque assumenda culpa veniam fugit optio eligendi magni sed. Corporis soluta harum voluptatibus sequi, architecto itaque quam voluptatum alias, consequatur aliquid blanditiis dignissimos. Totam porro quia iste id, nulla voluptates laboriosam et quidem soluta a eius quod iure tempore dignissimos placeat nihil amet ea necessitatibus eveniet suscipit doloribus similique? Laboriosam ea veritatis eaque. Obcaecati delectus quae ut msamus alias quis architecto voluptate velit officiis saepe odio, esse hic molestias repellendus neque numquam delectus, ad repellat minus nisi dolorem quos itaque. Voluptatem explicabo quibusdam sint ipsum libero, ipsam quam corporis vel tempora impedit adipisci numquam.ellendus neque numquam delectellendus neque numquam delect</p>
+        </div>
+        <div className="downMidBox">
+          <h2>HTML TRAINING3</h2>
+          <p><p style={{ color: 'red' }}>Lorem ipsum dolor sit</p> amet consetio laboriosam perferendis beatae dolores inventore fugiat, incidunt voluptates voluptatibus ullam natus animi unde eum atque perspiciatis maiores, suscipit doloribus, exercitationem amet consectetur optio quaerat accusamus qui! Officiis distinctio id odit, consequuntur excepturi minus obcaecati numquam vero perferendis ull, impedit omnis aperiam et illum repellendus veritatis accusamus? Ea soluta eveniet non praesentium sint, sapiente doloribus numquam, excepturi corrupti perspiciatis ut reiciendis atque deleniti voluptate harum veritatis sunt, animi sequi molestias provident assumenda aliquam consectetur? Deleniti officiis fuga suscipit quasi quo sunt velit recusandae ea excepturi, voluptas hic?</p>
+        </div>
+        <div className="downRightBox">
+          <p><p style={{ color: 'red' }}>nis aaperiam ab quibusdam ducimus necessitatibus similique doloremque! Eligendi, temporibus. Quasi, rep</p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, corporis. Porro recusandae, veritatis aliquid ducimus quo tenetur facere voluptatem velit magnam cum enim adipisci quae alias, molestias, fuga beatae perspiciatis sit harum corporis unde itaque maxime. Dignissimos omnis aaperiam ab quibusdam ducimus necessitatibus similique doloremque! Eligendi, temporibus. Quasi, repellat numquam dicta error sed similique a aliquam at ipsa dolores. Quae alias excepturi cum nobis odio, itaque laudantium in numquam ad ex repellat dicta. Atque, aliquid.</p>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
